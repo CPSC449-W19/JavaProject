48ee45562c733144b291cedff6a29d60ef216723
Elzanne1 <46723559+Elzanne1@users.noreply.github.com>
2019-02-09 20:48:04 -0700

    Delete InvalidTaskException.java

--- src/Exception/InvalidTaskException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-import java.lang.Exception;
-
-public class InvalidTaskException extends Exception {
-    public InvalidTaskException(String errorMessage) {
-        super(errorMessage);
-    }
-}
